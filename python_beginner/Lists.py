1b53605b5bfb691c283553a76bcaf1311db6dc75
Rohit Selote <workforrohitselote@gmail.com>
2026-01-02 06:10:16 +0530

    Modify dogs list with insertions and appends
    Added insertion of 'Test' and extended list with 'Test1' and 'Test2'.

--- python_beginner/Lists.py
+++ python_beginner/Lists.py
@@ -2,8 +2,10 @@ dogs=["roger",1,"syd",True,"Quincy",7]
 dogs[2]="beau"
 
 dogs += ["judah",5] #append
-
+dogs.insert(2,"Test")
 dogs.remove("Quincy")
+dogs[1:1]=["Test1","Test2"]
 print(dogs)
 print(dogs.pop())
+
 print(dogs)
