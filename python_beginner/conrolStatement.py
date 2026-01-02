c9f2d8093510120f67bedb6ba6f3adc377a7854a
Rohit Selote <workforrohitselote@gmail.com>
2026-01-02 05:42:24 +0530

    Add files via upload

---
+++ python_beginner/conrolStatement.py
@@ -0,0 +1,13 @@
+condition = False
+name="Roger"
+
+if condition == True:
+    print("The condition is True")
+elif name == "Beau" :
+    print("name is Beau")
+elif name == "syed" :
+    print("Hello Syed")
+elif name == "Roger":
+    print("Hello Roger")
+else:
+    print("The condition was false")
