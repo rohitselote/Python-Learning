199b4d886ca48d4c4bf6b387b4741d7f0748decd
Rohit Selote <workforrohitselote@gmail.com>
2026-01-03 10:26:12 +0530

    Enhance Lists.py with sorting examples
    Added list sorting functionality and demonstrated usage of sorted() and sort() methods.

--- python_beginner/Lists.py
+++ python_beginner/Lists.py
@@ -9,3 +9,28 @@ print(dogs)
 print(dogs.pop())
 
 print(dogs)
+
+#List Sorting
+items=["roger","Beau","syd","True","Quincy"]
+
+print(items)
+
+#using global function sorted 
+print(sorted(items,key=str.lower)) 
+
+
+itemscopy = items[:] #it will copy list at the instance
+
+
+
+#sorting not supported between instances of 'int' and 'str'
+items.sort()
+print(items) #in str their is also priority to words start from Capital letter.
+
+#to create non caring sorting about lower case and upper case
+items.sort(key=str.lower)
+print(items)
+ 
+
+#it will print list content copied at that instance.
+print(itemscopy)
