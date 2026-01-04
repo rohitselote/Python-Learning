040341140d532d02d1cfc33b793abb1051573691
Rohit Selote <workforrohitselote@gmail.com>
2026-01-04 11:08:33 +0530

    Add files via upload

---
+++ python_beginner/Tuples.py
@@ -0,0 +1,18 @@
+#Tupele
+name = ("Roger","Syd","Beau")
+print(name[0])
+print(name.index("Syd"))
+
+print(len(name))
+
+print("Roger" in name)
+
+print(sorted(name))
+
+#you cannot modify the tuple
+
+#you can create a new tuple from a existing tuple
+
+newTuple = name + ("Tina" ,"Quincy")
+
+print(newTuple)
