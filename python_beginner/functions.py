fd30a1b739e50f588c5e90b964d0c5d6e60e3cb3
Rohit Selote <workforrohitselote@gmail.com>
2026-01-13 21:11:47 +0530

    Implement hello and hola functions
    Added functions to greet users in different languages.

---
+++ python_beginner/functions.py
@@ -0,0 +1,14 @@
+#functions
+#a se of instrucion we can run when needed 
+def hello():
+    print('hello !')
+hello()
+def hola(name="amigo"):
+    #calling the functions using arguments
+    print("hola " + name)
+#we call the parameters the value accepted by
+#funcion inside the function defination
+hola("mark")
+#and arguments are values we pass to the function
+#we can set default value for a function
+hola()  
