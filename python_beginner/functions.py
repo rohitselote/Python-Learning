2737674d564e808162cee44e033f20ea2f999375
Rohit Selote <workforrohitselote@gmail.com>
2026-01-14 22:36:32 +0530

    Add greeting and variable scope functions
    Added functions to greet a user and demonstrate variable scope.

--- python_beginner/functions.py
+++ python_beginner/functions.py
@@ -12,3 +12,16 @@ hola("mark")
 #and arguments are values we pass to the function
 #we can set default value for a function
 hola()  
+def hi(name,age):
+    print("hi "+ name + " you are " +str(age) +" year old.")
+
+hi("mark",39)
+
+def change(value):
+    value=2 #it doesnt change anything outside  the function
+
+val=1
+
+print(change(val)) #it will print none
+
+print(val) #it will print 1
