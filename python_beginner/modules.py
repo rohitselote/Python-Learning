62f7782bba8cc63d22138251641cc34d7d2da60c
Rohit Selote <workforrohitselote@gmail.com>
2026-01-23 22:27:44 +0530

    Add module example for importing functions

---
+++ python_beginner/modules.py
@@ -0,0 +1,15 @@
+# every python file is module
+
+
+# import dog
+# dog.bark()
+
+#from dog  import bark
+#import specific fnction we need
+#bark()
+
+# from lib import dog
+# dog.bark()
+
+from lib.dog import bark
+bark()
