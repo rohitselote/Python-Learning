d46b2faa6fad44f983cd06a1edaafae73f331e43
Rohit Selote <workforrohitselote@gmail.com>
2026-01-24 20:39:22 +0530

    Implement command line argument handling
    Added basic example of handling command line arguments using sys module.

--- python_beginner/commandLineArguments.py
+++ python_beginner/commandLineArguments.py
@@ -1,3 +1,11 @@
 #accepting arguments
 # print("Hello")
 # Enter this command line in shell to run the program python commandLineArguments
+#Basic way to handle arguments to use  sys module from the standard libray
+import sys 
+print(sys.argv)
+# this is how we can print all the arguments thats  were passed in when we called the program 
+
+# python commandLineArguments.py rohit 166
+# it gonna print list of aruments
+# so this is baically list of arguments in string form
