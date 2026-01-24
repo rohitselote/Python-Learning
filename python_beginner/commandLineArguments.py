2eedd66a0dc9aa1f8c7311d68571ea0771834e8a
Rohit Selote <workforrohitselote@gmail.com>
2026-01-24 21:42:35 +0530

    Comment out command line argument handling code
    Comment out code for handling command line arguments.

--- python_beginner/commandLineArguments.py
+++ python_beginner/commandLineArguments.py
@@ -2,10 +2,14 @@
 # print("Hello")
 # Enter this command line in shell to run the program python commandLineArguments
 #Basic way to handle arguments to use  sys module from the standard libray
-import sys 
-print(sys.argv)
+# import sys 
+# print(sys.argv)
 # this is how we can print all the arguments thats  were passed in when we called the program 
 
 # python commandLineArguments.py rohit 166
 # it gonna print list of aruments
 # so this is baically list of arguments in string form
+# name=sys.argv[1]
+# print("Hello " + name)
+# command : python commandLineArguments.py beau
+# output : Hello beau
