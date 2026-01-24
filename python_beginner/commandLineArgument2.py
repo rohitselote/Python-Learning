061016242885ccd87e924cffbe281a240decd9ec
Rohit Selote <workforrohitselote@gmail.com>
2026-01-24 22:07:54 +0530

    Update commandLineArgument2.py

--- python_beginner/commandLineArgument2.py
+++ python_beginner/commandLineArgument2.py
@@ -4,8 +4,11 @@ parser=argparse.ArgumentParser(
     description='This program prints the name of my  dog'
 )
 
-parser.add_argument('-c','--color',metavar='color' ,required=True,help='the color to search for')
+# parser.add_argument('-c','--color',metavar='color' ,required=True,help='the color to search for')
 #python commandLineArgument2.py -c red
+
+parser.add_argument('-c','--color',metavar='color' ,required=True,choices={'red','yellow'},help='the color to search for')
+
 args = parser.parse_args()
 
 print(args.color)
