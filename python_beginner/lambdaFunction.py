dce65aa52324eaa267e5cb86a4c9844b723b21ca
Rohit Selote <workforrohitselote@gmail.com>
2026-01-25 12:32:52 +0530

    Update lambdaFunction.py

--- python_beginner/lambdaFunction.py
+++ python_beginner/lambdaFunction.py
@@ -1,1 +1,12 @@
 #Lambda Functions
+lambda num : num*2
+
+
+#lambda function can accepts more arguments
+# lambda a,b :a*b
+
+#lambda function can not be invoked directly
+#but you can assign them to variables
+multiply = lambda a,b :a*b
+
+print(multiply(2,4))
