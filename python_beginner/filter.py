ab9745f57a04bde8401304f61bc96b8d5be431da
Rohit Selote <workforrohitselote@gmail.com>
2026-01-26 17:47:44 +0530

    Update filter.py

--- python_beginner/filter.py
+++ python_beginner/filter.py
@@ -1,1 +1,10 @@
 #filter()
+numbers=[1,2,3]
+#filter takes an iterable and returns an iterrable object which is  another iterble 
+#but witout some of the original items so you can do  so byreurnin true or false
+#from the filter function
+# def isEven(n):
+#     return  n%2==0
+
+result=filter(lambda a : a%2==0 ,numbers)
+print(list(result))
