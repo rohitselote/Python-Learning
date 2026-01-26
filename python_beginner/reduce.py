260b53c67546b79454defbfe9b684e5b2b5afade
Rohit Selote <workforrohitselote@gmail.com>
2026-01-26 18:05:12 +0530

    Update reduce.py

--- python_beginner/reduce.py
+++ python_beginner/reduce.py
@@ -1,1 +1,11 @@
 #reduce()
+from functools import reduce
+expenses = [("dinner",180),("car repair",120)]
+
+# for expense in expenses:
+#     sum+=expense[1]
+# print(sum)   
+
+
+sum=reduce(lambda a,b : a[1] + b[1] , expenses)
+print(sum)
