dbc6ceae2bd8aa568961607deb8f1b095005a75d
Rohit Selote <workforrohitselote@gmail.com>
2026-01-26 14:19:55 +0530

    Update mapFilterReduce.py

--- python_beginner/mapFilterReduce.py
+++ python_beginner/mapFilterReduce.py
@@ -5,10 +5,13 @@
 #it is used to  run a function upon  each item in an iterable item in a list
 #and create a new list with same no. of items but the values of each item  can  be
 numbes=[1,2,3,4]
-def double(a):
-    return a*2
+# def double(a):
+#     return a*2
 
-result=map(double,numbes)
+# result=map(double,numbes)
+result=map(lambda a : a*2, numbes )
+
+#when the function is one  liner so its common too use lambda function
 
 print(list(result))
 #so whenever you want to do run a function n a each item in a list you can use map#map ,Filter , Reduce
