9b52ee2da93c5f24d6990b947ca06531fdd7354b
Rohit Selote <workforrohitselote@gmail.com>
2026-01-27 20:54:53 +0530

    Update decorator.py

--- python_beginner/decorator.py
+++ python_beginner/decorator.py
@@ -1,1 +1,15 @@
+#Decoators
+#whenever we call  the function the decorator is going to called
+#the decorator is function that takes a funcion ass a parmeter wraps the functioon in a
+#inner function that performs the job it has to do and returns that inner funcion 
+def logtime(func):
+    def wrapper():
+        print("Before")
+        val = func()
+        print("After")
+    return wrapper 
+@logtime
+def hello():
+    print("hello")
 
+hello()
