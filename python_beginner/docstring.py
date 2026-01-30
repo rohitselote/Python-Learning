02f7499148ad0ef38c6fc5b427b50be1d887878a
Rohit Selote <workforrohitselote@gmail.com>
2026-01-30 11:24:48 +0530

    Update docstring.py

--- python_beginner/docstring.py
+++ python_beginner/docstring.py
@@ -1,1 +1,15 @@
 
+#dostring
+"""Dog Module
+this module ---
+
+"""
+class Dog:
+    """A Class representing a dog"""
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def bark(self):
+        """Let the dog bark"""
+        print('wof')
+print(help(Dog))
