d4fb083d3418e516ce81e78264cec06f07fa71ed
Rohit Selote <workforrohitselote@gmail.com>
2026-01-31 23:55:37 +0530

    Update annnotation.py

--- python_beginner/annnotation.py
+++ python_beginner/annnotation.py
@@ -4,3 +4,13 @@
 #so we do not have to specify the type of a variable or
 #parameter or a function return value 
 #annotations allows us to optionally do that
+def increment(n: int) -> int:
+#we are going to specify that this function receives
+# an int and then its also going to return an end 
+# you can do the same things with variables
+    return n+1
+
+count :int = 0
+#we are specifying that this integer goiing to be an integer
+# python will atually ignore this annotation a seperate tool 
+# #mypi can be run stand alone or integreted by IDEs  
