2affc85e4782a5a4c1e9dfdd3d99d66e1cb50158
Rohit Selote <workforrohitselote@gmail.com>
2026-01-31 23:40:05 +0530

    Update annnotation.py

--- python_beginner/annnotation.py
+++ python_beginner/annnotation.py
@@ -1,1 +1,6 @@
 
+#annotations
+#python is dynamically typed
+#so we do not have to specify the type of a variable or
+#parameter or a function return value 
+#annotations allows us to optionally do that
