fb13b308c3727d5d9bcb20afcfbd396fcd181d0f
Rohit Selote <workforrohitselote@gmail.com>
2026-02-01 15:43:22 +0530

    Document exception handling with comments
    Added comments explaining exception handling in Python.

--- python_beginner/exceptions.py
+++ python_beginner/exceptions.py
@@ -2,3 +2,15 @@
 #so for exception handling you would wrap lines of code in a try block
 #and inside the block you will put the lines of code then if an error occurs
 #python will alert you and you can determine which kind of error occured using an except block 
+#try:
+    #some lines of code
+#except <ERROR1>:
+    #handler <ERROR1>
+#except <ERROR2>:
+    #handler <ERROR2>
+#else:
+    #it will run if no exceptions are found
+
+#finally:
+    #always run at the end whether there are
+    # exceptions or not 
