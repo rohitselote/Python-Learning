949bcf7c913e689f027a454cb82895b9139bc4f5
Rohit Selote <workforrohitselote@gmail.com>
2026-02-02 22:54:34 +0530

    Enhance listCompression.py with comments on usage
    Added comments explaining list comprehension and its advantages.

---
+++ listCompression.py
@@ -0,0 +1,10 @@
+#List Compression
+#List Compression
+#its a way to create a list in a concise way to create a list 
+numbers=[1,2,3,4,5]
+
+nos_power=[n**2 for n in numbers]
+
+#list compresion are the syntax that sometimes preffered over  loops
+#more readable when operations can be written on a single line
+print(nos_power)
