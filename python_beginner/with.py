eb28f849ea08bbc6d99681af2805fce42886e4f0
Rohit Selote <workforrohitselote@gmail.com>
2026-02-02 21:52:55 +0530

    Enhance with.py with comments on exception handling
    Added comments explaining the use of the 'with' statement for file handling.

--- python_beginner/with.py
+++ python_beginner/with.py
@@ -1,1 +1,17 @@
 #with
+#with statememnt is very helpful to simplify working
+#with exception handling
+
+
+filename='hello.txt'
+
+# try:
+#     file = open(filename,'r')
+#     content = file.read()
+# finally:
+#     file.close()
+
+#an alternate way to do this
+with open(filename,'r') as file:
+    content=file.read()
+    print(content)
