c73955d090d82e26e556c44c857113a3637ef262
Rohit Selote <workforrohitselote@gmail.com>
2026-02-02 22:06:21 +0530

    Create insPackWithPIP.py for pip usage instructions
    Add instructions for installing and uninstalling packages with pip

---
+++ python_beginner/insPackWithPIP.py
@@ -0,0 +1,12 @@
+#installing third party packages using pip
+
+#type command in shell to install packages
+#such that
+# pip install requests
+
+#for latest vesion 
+#pip install -U requests
+
+
+#for uninstalling
+# pip uninstall requests
