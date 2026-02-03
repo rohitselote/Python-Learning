d972d832ef5cde07efaa334a32593b1dee2fbc72
Rohit Selote <workforrohitselote@gmail.com>
2026-02-03 22:57:36 +0530

    Add Dog and Cat classes to illustrate polymorphism
    Demonstrate polymorphism with Dog and Cat classes implementing the same method.

--- python_beginner/polymorphism.py
+++ python_beginner/polymorphism.py
@@ -1,1 +1,20 @@
 #Polymorphism
+
+class Dog:
+    def eat(self):
+        print("eating dog food")
+
+class Cat:
+    def eat(self):
+        print("eating cat food")
+
+animal1=Dog()
+animal2=Cat()
+
+#different classes to share the same method name while having unique implementations
+
+animal1.eat()
+animal2.eat()
+
+#we build a generalized interface and 
+#we do not need to know that animal is a cat o dog 
